ac8d66402b2e40e71002843d7dfdee3da020746d
chenjh <1063517498@qq.com>
2017-05-23 09:15:35 +0800

    1、提交项目

---
+++ src/test/java/Test1.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created by lenovo on 2017/5/16.
+ */
+public class Test1 {
+
+    @Test
+    public void test11() {
+        System.out.println("你好，异界");
+    }
+}
